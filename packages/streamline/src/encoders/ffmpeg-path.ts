6c5d42e742657b440f2cd9b2d1d9e7413c318534
Robbin "Roboroads" Schepers <robbin@deschepers.nl>
2026-06-02 13:41:16 +0200

    test: cover ffmpeg-path
    Signed-off-by: Robbin "Roboroads" Schepers <robbin@deschepers.nl>

--- packages/streamline/src/encoders/ffmpeg-path.ts
+++ packages/streamline/src/encoders/ffmpeg-path.ts
@@ -1,15 +1,14 @@
 import { app } from 'electron';
+import ffmpegStatic from 'ffmpeg-static';
 
 let _path: string | null = null;
 
 export function getFFmpegPath(): string {
 	if (_path) return _path;
-	// eslint-disable-next-line @typescript-eslint/no-require-imports
-	const ffmpegStatic: string = require('ffmpeg-static');
 	if (app.isPackaged) {
-		_path = ffmpegStatic.replace('app.asar', 'app.asar.unpacked');
+		_path = ffmpegStatic!.replace('app.asar', 'app.asar.unpacked');
 	} else {
-		_path = ffmpegStatic;
+		_path = ffmpegStatic!;
 	}
 	return _path!;
 }
